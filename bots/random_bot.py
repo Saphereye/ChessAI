3b80a62b4a20a8192f4bfe8a6fa313a1f1511ba3
Saphereye <adarshdas950@gmail.com>
2023-11-12 13:17:02 +0530

    feat: Added evaluation function to main

--- bots/random_bot.py
+++ bots/random_bot.py
@@ -5,9 +5,4 @@ import random
 
 class RandomBot(BaseBot):
     def move(self, board: Board) -> Move | None:
-        legal_moves = list(board.legal_moves)
-        if len(legal_moves) < 1:
-            return None
-        move = random.choice(list(board.legal_moves))
-        print(move)
-        return move
+        return random.choice(list(board.legal_moves))
