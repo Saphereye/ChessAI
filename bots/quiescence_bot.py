8c18a2ed195fb35dcfb04b4aaa8982c5d5a8e36a
Kush <kushpagrawal@gmail.com>
2023-11-19 18:36:35 +0530

    QuiescenceBot added

--- bots/quiescence_bot.py
+++ bots/quiescence_bot.py
@@ -7,9 +7,8 @@ import random
 import math
 
 class QuiescenceBot(BaseBot):
-    def __init__(self, max_depth: int = 4) -> None:
-        self.max_depth = max_depth
-        self.memo = {}
+    def __init__(self, max_depth: int = 3) -> None:
+        super().__init__(max_depth)
 
     def move(self, board: Board) -> Move | None:
         legal_moves = list(board.legal_moves)
@@ -19,7 +18,7 @@ class QuiescenceBot(BaseBot):
 
         for move in legal_moves:
             board.push(move)
-            eval_score = self.quiescence(board, self.max_depth - 1, alpha, beta, False)
+            eval_score = self.alpha_beta(board, self.max_depth - 1, alpha, beta, False)
             board.pop()
 
             if eval_score > alpha:
@@ -28,9 +27,29 @@ class QuiescenceBot(BaseBot):
 
         return best_move
 
-    def quiescence(self, board, depth, alpha, beta, maximizing_player):
+    def quiescence_search(self, board, alpha, beta):
+        stand_pat = evaluate_board(board)
+        if stand_pat >= beta:
+            return beta
+        if alpha < stand_pat:
+            alpha = stand_pat
+
+        legal_captures = [move for move in board.legal_moves if board.is_capture(move)]
+        for move in legal_captures:
+            board.push(move)
+            score = -self.quiescence_search(board, -beta, -alpha)
+            board.pop()
+
+            if score >= beta:
+                return beta
+            if score > alpha:
+                alpha = score
+
+        return alpha
+
+    def alpha_beta(self, board, depth, alpha, beta, maximizing_player):
         if depth == 0 or board.is_game_over():
-            return evaluate_board(board)
+            return self.quiescence_search(board, alpha, beta)
 
         legal_moves = list(board.legal_moves)
 
@@ -38,7 +57,7 @@ class QuiescenceBot(BaseBot):
             max_eval = float('-inf')
             for move in legal_moves:
                 board.push(move)
-                eval_score = self.quiescence(board, depth - 1, alpha, beta, False)
+                eval_score = self.alpha_beta(board, depth - 1, alpha, beta, False)
                 board.pop()
                 max_eval = max(max_eval, eval_score)
                 alpha = max(alpha, eval_score)
@@ -46,19 +65,13 @@ class QuiescenceBot(BaseBot):
                     break
             return max_eval
         else:
-            stand_pat = evaluate_board(board)
-            if stand_pat >= beta:
-                return beta
-            if alpha < stand_pat:
-                alpha = stand_pat
+            min_eval = float('inf')
             for move in legal_moves:
-                if board.is_capture(move):
-                    board.push(move)
-                    eval_score = self.quiescence(board, depth - 1, alpha, beta, True)
-                    board.pop()
-                    if eval_score >= beta:
-                        return beta
-                    if eval_score > alpha:
-                        alpha = eval_score
-
-            return alpha
+                board.push(move)
+                eval_score = self.alpha_beta(board, depth - 1, alpha, beta, True)
+                board.pop()
+                min_eval = min(min_eval, eval_score)
+                beta = min(beta, eval_score)
+                if beta <= alpha:
+                    break
+            return min_eval
